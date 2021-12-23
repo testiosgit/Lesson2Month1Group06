6ed68363b29119e5f74d10eb59ff7b2d0f27ed5a
testiosgit <java1geektech@gmail.com>
2021-12-23 20:57:39 +0600

    Initial Commit

---
+++ Lesson2Month1Group06/main.swift
@@ -0,0 +1,125 @@
+//
+//  main.swift
+//  Lesson2Month1Group06
+//
+//  Created by User on 12/23/21.
+//
+
+import Foundation
+
+//var firstName: String = "Ariet"
+//var lastName: String = "Abdisamat uulu"
+//
+//var birthYear = 1997
+//var currentYear = 2021
+//
+//func showInfo(){
+//    let age = currentYear - birthYear
+//
+//    print("\(lastName) \(firstName), \(age) лет")
+//}
+//
+//showInfo()
+//
+//
+//print("Здравствуйте, как вас зовут?")
+//var text = readLine()!
+//print("Приветствую вас \(text)")
+
+//print("Cколько вам лет?")
+//var age = 70
+//
+//// >, <, ==, <=, =>
+//if (age > 63) {
+//    print("нужно выходить на пенсию")
+//}
+//
+//if(age >= 18) {
+//    print("Нам можно получить водительские права")
+//}
+
+//print("Введите имя:")
+//var name = readLine()!
+//
+//switch name {
+//case "Nursultan":
+//    print(27)
+//case "Beksultan":
+//    print(17)
+//case "Ariet":
+//print(24)
+//case "Bakyt":
+//    print(23)
+//case "Islam":
+//print(26)
+//
+//default:
+//    print("Такого нет!")
+//}
+
+print("Введите время сезона:")
+var season = readLine()!
+
+
+if (season == "summer"){
+    print("лето")
+}else if (season == "winter"){
+    print("зима")
+}else if(season == "spring"){
+    print("весна")
+}else if(season == "autumn"){
+    print("осень")
+}
+
+switch season {
+case "summer":
+    print("лето")
+case "winter":
+    print("зима")
+case "autumn":
+    print("осень")
+case "spring":
+    print("весна")
+default:
+    print("другое")
+}
+
+
+//print("Введите год рождения:")
+//var year = Int(readLine()!)
+//
+//print("Введите текущий год:")
+//var year2 = Int(readLine()!)
+//
+//let age = Int(year2 ?? 0) - Int(year ?? 0)
+//
+//if(age == 27 && age == 26 && age == 2) {
+//    print(age)
+//} else {
+//    print("Вы ввели неверные данные")
+//}
+//
+//
+//switch age {
+//case 27:
+//    print(age)
+//case 26:
+//    print(age)
+//case 25
+//default:
+//    print("Вы ввели неверные данные")
+//}
+
+
+
+//var time1 = 21
+//var time2 = 5
+
+//if (time2 >= 6 || time1 <= 21 && ||){
+//    print("цена проезда 10 сом")
+//}else if () {
+//    print("цена проезда 12 сом")
+//}
+
+
+
